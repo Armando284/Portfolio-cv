c81c87fcfe13110f874a2b3fe22ed84fe6d9ad40
Diegu <diegudeveloper@gmail.com>
2021-05-11 23:42:31 +0200

    Configuracion del container resumen y efecto en javascript

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btnAboutme = document.getElementById("btn-aboutme");
     btnAboutme.addEventListener("click", sliderAbautMe);
     btnHome = document.getElementById("btn-home");
+    btnHome.addEventListener("click", sliderhome);
+    btnResumen = document.getElementById("btn-resume");
+    btnResumen.addEventListener("click", sliderresumen);
 
 function sliderAbautMe () {
     let infoabout = document.getElementById("infabout");
@@ -12,4 +15,9 @@ function sliderhome () {
     let infohome = document.getElementById("infohome");
     //infoabout.style.transform = "translatex(0)"
     infohome.style.left=(100);
+}
+function sliderresumen () {
+    let inforesumen = document.getElementById("inforesumen");
+    //infoabout.style.transform = "translatex(0)"
+    inforesumen.style.left=(0);
 }
